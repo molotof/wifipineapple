048297fe4d0dec1d588752fc411ef01b102a7c79
Sebastian Kinne <sebkinne@gmail.com>
2012-03-19 10:29:24 +0000

    Fixed grepping of interface IPs

--- pineapple/index.php
+++ pineapple/index.php
@@ -69,8 +69,8 @@ echo "&nbsp; &nbsp; &nbsp; SSH <font color=\"lime\"><b>online</b></font>. &nbsp;
                                                                                                                                                         
 echo "<br/><b>Interfaces</b><br />";
 
-echo "&nbsp;PoE / LAN Port: " . exec("ifconfig br-lan | grep inet | awk '{print $2}' | cut -c6-16") . "<br />";
-echo "&nbsp;&nbsp; USB 3G Modem: " . exec("ifconfig 3g-wan2 | grep inet | awk '{print $2}' | cut -c6-16") . "<br />";
+echo "&nbsp;PoE / LAN Port: " . exec("ifconfig br-lan | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'") . "<br />";
+echo "&nbsp;&nbsp; USB 3G Modem: " . exec("ifconfig 3g-wan2 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'") . "<br />";
 echo "&nbsp;WAN / LAN Port: " . exec("ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'") . "<br />";
 echo "Public Internet: "; 
 if (isset($_GET[revealpublic])) { 
